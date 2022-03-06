c09d0c9bbe6a03c0f08210e6fa4ea1e6bffcc3ec
Arz <17147640+Arsalan-A@users.noreply.github.com>
2022-03-06 00:02:48 -0500

    added api call

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const REDDIT_API = 'https://www.reddit.com';
+
+export default async function fetchSubreddit() {
+  const response = await fetch(`${REDDIT_API}/subreddits.json`);
+  const result = await response.json();
+
+  return result.data.children.map((subreddit) => subreddit.data);
+}
+
+//Subreddit Posts Api
+`https://www.reddit.com${subreddit}.json`;
